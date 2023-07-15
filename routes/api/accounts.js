f8a0219973a1ae22f63fdf8efbbc8e22db4175cb
damienchoojl <damienchoojl@gmail.com>
2023-07-15 13:03:10 +0800

    Revert "added new routes"
    This reverts commit 33abddc504e27c8c147c0cebc6163e08d322debb.

--- routes/api/accounts.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const accountsCtrl = require("../../controllers/api/accounts");
-const ensureLoggedIn = require("../../config/ensureLoggedIn");
-
-router.get("/", ensureLoggedIn, accountsCtrl.getAccount);
-router.put("/pets", ensureLoggedIn, accountsCtrl.updatePets);
-
-module.exports = router;
