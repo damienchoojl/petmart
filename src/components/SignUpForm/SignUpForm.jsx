3f53fb6ba1957c76665680ebb11f9cc37a71d6b0
damienchoojl <damienchoojl@gmail.com>
2023-07-16 12:59:32 +0800

    updated signup and login form
    direct to mainpage after signup or login

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,91 +1,95 @@
-import { Component } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { signUp } from "../../utilities/users-service";
 
-export default class SignUpForm extends Component {
-  state = {
+export default function SignUpForm({ setUser }) {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
     confirm: "",
     error: "",
-  };
+  });
 
-  // The object passed to setState is merged with the current state object
-  handleChange = (evt) => {
-    this.setState({
+  const handleChange = (evt) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
       [evt.target.name]: evt.target.value,
       error: "",
-    });
+    }));
   };
 
-  handleSubmit = async (evt) => {
+  const handleSubmit = async (evt) => {
     // Prevent form from being submitted to the server
     evt.preventDefault();
 
     try {
       // We don't want to send the 'error' or 'confirm' property,
-      //  so let's make a copy of the state object, then delete them
-      const formData = { ...this.state };
-      delete formData.error;
-      delete formData.confirm;
+      // so let's make a copy of the state object, then delete them
+      const cleanedFormData = { ...formData };
+      delete cleanedFormData.error;
+      delete cleanedFormData.confirm;
 
       // The promise returned by the signUp service method
       // will resolve to the user object included in the
       // payload of the JSON Web Token (JWT)
-      const user = await signUp(formData);
-      // Baby step!
-      this.props.setUser(user);
+      const user = await signUp(cleanedFormData);
+      setUser(user);
+      navigate("/mainpage"); // Navigate to /mainpage after successful sign up
     } catch {
       // An error occurred
-      this.setState({ error: "Sign Up Failed - Try Again" });
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        error: "Sign Up Failed - Try Again",
+      }));
     }
   };
 
-  render() {
-    const disable = this.state.password !== this.state.confirm;
-    return (
-      <div>
-        <div className="form-container">
-          <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Confirm</label>
-            <input
-              type="password"
-              name="confirm"
-              value={this.state.confirm}
-              onChange={this.handleChange}
-              required
-            />
-            <button type="submit" disabled={disable}>
-              SIGN UP
-            </button>
-          </form>
-        </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
+  const disable = formData.password !== formData.confirm;
+
+  return (
+    <div>
+      <div className="form-container">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <label>Confirm</label>
+          <input
+            type="password"
+            name="confirm"
+            value={formData.confirm}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" disabled={disable}>
+            SIGN UP
+          </button>
+        </form>
       </div>
-    );
-  }
+      <p className="error-message">&nbsp;{formData.error}</p>
+    </div>
+  );
 }
