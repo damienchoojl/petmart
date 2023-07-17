6b8cda3c731e5a82319e3f3b6e0e96cc09f8c29d
damienchoojl <damienchoojl@gmail.com>
2023-07-17 15:18:54 +0800

    Update MainPage.jsx
    added carousel for mainpage as well as displaying of item images

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Carousel } from "antd";
-import ItemCard from "../ItemCard/ItemCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
-export default function MainPage() {
+const MainPage = () => {
   const [items, setItems] = useState([]);
-
   const carouselSettings = {
     dots: true,
     infinite: true,
@@ -39,7 +38,6 @@ export default function MainPage() {
 
   return (
     <div>
-      {/* Main Carousel to display poster */}
       <div className="items-carousel">
         <p></p>
         {itemsWithStock.length > 0 ? (
@@ -47,16 +45,139 @@ export default function MainPage() {
             {itemsWithStock.map((item) => (
               <Link
                 key={item._id}
-                to={`/items/${encodeURIComponent(item._id)}`}
+                to={`/items/${encodeURIComponent(item.name)}`}
+                state={item}
+                style={{ pointerEvents: "none" }}
               >
-                <ItemCard item={item} />
+                <img
+                  src={item.image1}
+                  alt={item.name}
+                  style={{
+                    border: "2px solid black",
+                    borderColor: "solid black",
+                    objectFit: "cover",
+                    maxWidth: "90%",
+                  }}
+                />
               </Link>
             ))}
           </Carousel>
         ) : (
-          <p>No items available for the carousel</p>
+          <p>No movies available for the carousel</p>
+        )}
+      </div>
+
+      {/* Cat Food */}
+      <div className="cat-food-carousel">
+        <h2>Cat Food</h2>
+        <hr></hr>
+        {items.length > 4 ? (
+          <Slider slidesToShow={4} slidesToScroll={1}>
+            {items
+              .filter((item) => item.petType === "Cat")
+              .map((item) => (
+                <div key={item.name} className="cat-item">
+                  <Link
+                    to={`/items/${encodeURIComponent(item.name)}`}
+                    state={item}
+                  >
+                    <img
+                      src={item.image1}
+                      alt={item.name}
+                      style={{
+                        maxWidth: "200px",
+                        maxHeight: "300px",
+                        border: "2px solid black",
+                        borderColor: "solid black",
+                      }}
+                    />
+                  </Link>
+                </div>
+              ))}
+          </Slider>
+        ) : (
+          <div className="cat-row">
+            {items
+              .filter((item) => item.petType === "Cat")
+              .map((item) => (
+                <div key={item.name} className="cat-item">
+                  <Link
+                    to={`/items/${encodeURIComponent(item.name)}`}
+                    state={item}
+                  >
+                    <img
+                      src={item.image1}
+                      alt={item.name}
+                      style={{
+                        maxWidth: "200px",
+                        maxHeight: "300px",
+                        border: "2px solid red",
+                        borderColor: "solid red",
+                      }}
+                    />
+                  </Link>
+                </div>
+              ))}
+          </div>
+        )}
+      </div>
+
+      {/* Dog Food */}
+      <div className="cat-food-carousel">
+        <h2>Dog Food</h2>
+        <hr></hr>
+        {items.length > 4 ? (
+          <Slider slidesToShow={4} slidesToScroll={1}>
+            {items
+              .filter((item) => item.petType === "Dog")
+              .map((item) => (
+                <div key={item.name} className="dog-item">
+                  <Link
+                    to={`/items/${encodeURIComponent(item.name)}`}
+                    state={item}
+                  >
+                    <img
+                      src={item.image1}
+                      alt={item.name}
+                      style={{
+                        maxWidth: "200px",
+                        maxHeight: "300px",
+                        border: "2px solid black",
+                        borderColor: "solid black",
+                      }}
+                    />
+                  </Link>
+                </div>
+              ))}
+          </Slider>
+        ) : (
+          <div className="dog-row">
+            {items
+              .filter((item) => item.petType === "Dog")
+              .map((item) => (
+                <div key={item.name} className="dog-item">
+                  <Link
+                    to={`/items/${encodeURIComponent(item.name)}`}
+                    state={item}
+                  >
+                    <img
+                      src={item.image1}
+                      alt={item.name}
+                      style={{
+                        maxWidth: "200px",
+                        maxHeight: "300px",
+                        border: "2px solid red",
+                        borderColor: "solid red",
+                      }}
+                    />
+                  </Link>
+                </div>
+              ))}
+          </div>
         )}
       </div>
     </div>
   );
-}
+};
+
+export default MainPage;
