32fc2abb61bfd2ba728ade6dc94f6ceed113057f
damienchoojl <damienchoojl@gmail.com>
2023-07-18 11:38:26 +0800

    added css for NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import * as userService from "../../utilities/users-service";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// import "./NavBar.css";
+import "../NavBar/NavBar.css";
 
 export default function NavBar({ user, setUser }) {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -20,12 +20,14 @@ export default function NavBar({ user, setUser }) {
   return (
     <nav className="navbar">
       <div className="nav-left">
-        <Link to="/mainpage" className="nav-logo">
-          <img
-            src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/wexaikkloejtsgnng6wy"
-            alt="Logo"
-          />
-        </Link>
+        <div className="nav-logo-container">
+          <Link to="/mainpage" className="nav-logo">
+            <img
+              src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/wexaikkloejtsgnng6wy"
+              alt="Logo"
+            />
+          </Link>
+        </div>
         <div className="nav-dropdown">
           <button onClick={handleDropdownToggle} className="nav-dropdown-btn">
             Pets
@@ -33,7 +35,6 @@ export default function NavBar({ user, setUser }) {
           {showDropdown && (
             <div className="nav-dropdown-content">
               <Link to="/cat">Cat</Link>
-              &nbsp;&nbsp;
               <Link to="/dog">Dog</Link>
             </div>
           )}
@@ -45,11 +46,10 @@ export default function NavBar({ user, setUser }) {
       <div className="nav-right">
         <Link to="/orders">Order History</Link>
         &nbsp; | &nbsp; <Link to="/orders/new">New Order</Link>
-        &nbsp;&nbsp;<span>Welcome, {user.name}</span>
-        &nbsp;&nbsp;
+        <span>Welcome, {user.name}</span>
         <Link to="/favourites" className="nav-link">
           <FavoriteIcon
-            sx={{ color: "white" }}
+            sx={{ color: "grey" }}
             style={{
               width: "20px",
               height: "20px",
@@ -57,10 +57,9 @@ export default function NavBar({ user, setUser }) {
             }}
           />
         </Link>
-        &nbsp;&nbsp;
         <Link to="/cart" className="nav-link">
           <ShoppingCartIcon
-            sx={{ color: "white" }}
+            sx={{ color: "grey" }}
             style={{
               width: "20px",
               height: "20px",
@@ -68,7 +67,6 @@ export default function NavBar({ user, setUser }) {
             }}
           />
         </Link>
-        &nbsp;&nbsp;
         <Link to="" onClick={handleLogOut} className="nav-link">
           Log Out
         </Link>
