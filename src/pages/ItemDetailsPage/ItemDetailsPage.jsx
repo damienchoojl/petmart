cba3723a1af7156ce3864a71107c9b3b0a0fa992
damienchoojl <damienchoojl@gmail.com>
2023-07-18 14:06:32 +0800

    tidying ItemDetailsPage with css

--- src/pages/ItemDetailsPage/ItemDetailsPage.jsx
+++ src/pages/ItemDetailsPage/ItemDetailsPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "../ItemDetailsPage/ItemDetailsPage.css";
 
 export default function ItemDetailsPage() {
   const [item, setItem] = useState(null);
@@ -61,52 +62,74 @@ export default function ItemDetailsPage() {
   return (
     <div>
       {item ? (
-        <div key={item._id}>
-          <img src={item.image1} alt={item.name} />
-          <h2>{item.name}</h2>
-          <p>Details: {item.details[0].details}</p>
-          <p>
-            Ingredients:{" "}
-            {Array.isArray(item.details[0].ingredients)
-              ? item.details[0].ingredients.join(", ")
-              : item.details[0].ingredients}
-          </p>
-          <p>Price: ${item.price.toFixed(2)}</p>
-          <p>Stocks: {item.remainStock}</p>
-          <div>
-            <button onClick={handleDecrement} disabled={quantity === 1}>
-              -
-            </button>
-            <span>{quantity}</span>
-            <button
-              onClick={handleIncrement}
-              disabled={quantity === item.remainStock}
-            >
-              +
-            </button>
-          </div>
-          <button onClick={handleAddToCart} disabled={item.remainStock <= 0}>
-            Add to Cart
-          </button>
-          {item.remainStock <= 0 && <p>Out of Stock</p>}
-          {addedToCart && <p>Added to cart successfully</p>}
-          {item.comments.length > 0 && (
-            <div>
-              <h3>Comments:</h3>
-              <Slider {...carouselSettings}>
-                {item.comments.map((comment) => (
-                  <div key={comment._id}>
-                    <p>User ID: {comment.userId._id}</p>
-                    <p>Rating: {comment.rating}</p>
-                    <p>Comment: {comment.comment}</p>
-                  </div>
-                ))}
-              </Slider>
+        <div className="item-details-container">
+          <img src={item.image1} alt={item.name} className="item-image" />
+          <div className="item-details">
+            <h2 className="item-name">{item.name}</h2>
+            <p className="item-price">Price: ${item.price.toFixed(2)}</p>
+            <div className="quantity-container">
+              Quantity
+              <button
+                className="quantity-button"
+                onClick={handleDecrement}
+                disabled={quantity === 1}
+              >
+                -
+              </button>
+              <span>{quantity}</span>
+              <button
+                className="quantity-button"
+                onClick={handleIncrement}
+                disabled={quantity === item.remainStock}
+              >
+                +
+              </button>
+              <button
+                className="add-to-cart-button"
+                onClick={handleAddToCart}
+                disabled={item.remainStock <= 0}
+              >
+                Add to Cart
+              </button>
             </div>
-          )}
+            {item.remainStock <= 0 && <p>Out of Stock</p>}
+            {addedToCart && <p>Added to cart successfully</p>}
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
+      )}
+      <hr></hr>
+      {item && (
+        <div className="product-details-container">
+          <div className="product-details">
+            <h3>Details:</h3>
+            <p>{item.details[0].details}</p>
+          </div>
+          <div className="product-details">
+            <h3>Ingredients:</h3>
+            <p>
+              {Array.isArray(item.details[0].ingredients)
+                ? item.details[0].ingredients.join(", ")
+                : item.details[0].ingredients}
+            </p>
+          </div>
+        </div>
+      )}
+      <hr></hr>
+      {item && item.comments.length > 0 && (
+        <div className="comments-container">
+          <h3>Comments:</h3>
+          <Slider {...carouselSettings}>
+            {item.comments.map((comment) => (
+              <div key={comment._id}>
+                <p>User ID: {comment.userId._id}</p>
+                <p>Rating: {comment.rating}</p>
+                <p>Comment: {comment.comment}</p>
+              </div>
+            ))}
+          </Slider>
+        </div>
       )}
     </div>
   );
