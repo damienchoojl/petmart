853972b54cee4f730ce3c240177f8deefa4dca84
damienchoojl <damienchoojl@gmail.com>
2023-07-18 12:12:30 +0800

    tidying NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -44,8 +44,8 @@ export default function NavBar({ user, setUser }) {
         </Link>
       </div>
       <div className="nav-right">
-        <Link to="/orders">Order History</Link>
-        &nbsp; | &nbsp; <Link to="/orders/new">New Order</Link>
+        {/* <Link to="/orders">Order History</Link>
+        &nbsp; | &nbsp; <Link to="/orders/new">New Order</Link> */}
         <span>Welcome, {user.name}</span>
         <Link to="/favourites" className="nav-link">
           <FavoriteIcon
