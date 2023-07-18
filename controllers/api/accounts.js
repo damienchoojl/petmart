c1124acffc3a3f537993dec65ab84e95a287a791
damienchoojl <damienchoojl@gmail.com>
2023-07-18 16:45:46 +0800

    added controllers and routes to get cart item

--- controllers/api/accounts.js
+++ controllers/api/accounts.js
@@ -14,31 +14,21 @@ const getAccounts = async (req, res) => {
 const addToCart = async (req, res) => {
   try {
     const { userId, itemId, quantity } = req.body;
-
     const userAccount = await Account.findOne({ user: userId });
-
     if (!userAccount) {
       return res.status(404).json({ error: "User account not found" });
     }
-
     const itemToAdd = await Item.findOne({ _id: itemId });
-
     if (!itemToAdd) {
       return res.status(404).json({ error: "Item not found" });
     }
-
-    // Check if there is enough stock of the item
     if (quantity > itemToAdd.remainStock) {
       return res.status(400).json({ error: "Not enough stock" });
     }
-
-    // Add the item multiple times based on the quantity
     for (let i = 0; i < quantity; i++) {
       userAccount.itemInCart.push(itemId);
     }
-
     await userAccount.save();
-
     res.status(200).json({ message: "Item(s) added to cart successfully" });
   } catch (error) {
     console.error("Failed to add item to cart:", error);
@@ -46,7 +36,34 @@ const addToCart = async (req, res) => {
   }
 };
 
+const getCartItems = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+
+    const userId = req.user._id;
+    if (!userId) {
+      return res.status(400).json({ error: "Invalid user ID" });
+    }
+
+    const account = await Account.findOne({ user: userId }).populate(
+      "itemInCart"
+    );
+    if (!account) {
+      return res.status(404).json({ error: "User account not found" });
+    }
+
+    const cartItems = account.itemInCart;
+    res.status(200).json(cartItems);
+  } catch (error) {
+    console.error("Failed to fetch cart items:", error);
+    res.status(500).json({ error: "Failed to fetch cart items" });
+  }
+};
+
 module.exports = {
   getAccounts,
   addToCart,
+  getCartItems,
 };
--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
-// Import the controller for handling account routes
 const accountsCtrl = require("../../controllers/api/accounts");
 
-// Define the route handlers
 router.get("/", accountsCtrl.getAccounts);
 router.post("/add-to-cart", accountsCtrl.addToCart);
+router.get("/get-cart-items", accountsCtrl.getCartItems);
 
 module.exports = router;
