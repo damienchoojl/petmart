8f70019b0a7b660b9906ebf84dbda4922051c711
damienchoojl <damienchoojl@gmail.com>
2023-07-21 13:19:51 +0800

    display orderid and item details after checkout

--- src/pages/PurchasedConfirmationPage/PurchasedConfirmationPage.jsx
+++ src/pages/PurchasedConfirmationPage/PurchasedConfirmationPage.jsx
@@ -1,3 +1,33 @@
-export default function PurchasedConfirmationPage() {
-  return <div>Purchased Item</div>;
+import { Link, useLocation } from "react-router-dom";
+
+export default function PurchasedConfirmationPage({ user }) {
+  const location = useLocation();
+  const orderId = location.state?.orderId;
+  const purchasedItems = location.state?.items || [];
+
+  return (
+    <div className="orderConfirmation">
+      <div className="orderBorderBottom">
+        <h1>Order has been Confirmed!</h1>
+        <p>Hi {user.name}, Thank you for your purchase!</p>
+        <br />
+        <h2>Order Details</h2>
+        <p>Order ID: {orderId}</p>
+        <ul>
+          {purchasedItems.map((item) => (
+            <li key={item._id}>
+              {item.name} - Quantity: {item.quantity} - Amount: $
+              {(item.price * item.quantity).toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <br />
+      <Link to={"/mainpage"}>
+        <div className="ticketMainButton">
+          <button>Return to Main</button>
+        </div>
+      </Link>
+    </div>
+  );
 }
