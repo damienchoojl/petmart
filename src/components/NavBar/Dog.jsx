f969e2c7e524393f7fe13b0bb635d850c07c2b89
damienchoojl <damienchoojl@gmail.com>
2023-07-21 14:16:44 +0800

    added css for Dog section that allows user to shop by categories

--- src/components/NavBar/Dog.jsx
+++ src/components/NavBar/Dog.jsx
@@ -1,20 +1,39 @@
 import { Link } from "react-router-dom";
+import "./Dog.css";
 
 export default function Dog() {
   return (
-    <>
-      <div>
-        <h2>Shop by Categories</h2>
+    <div className="dog-container">
+      <h2>Shop by Categories</h2>
+      <div className="category-buttons">
         <Link to="/dog/food">
-          <button>Food</button>
+          <button className="category-button">
+            <img
+              src="https://cdn.shopify.com/s/files/1/1149/5008/files/1_e4ac6ae2-c563-4d27-a23e-f8a38f34d26c.png"
+              alt="Food"
+            />
+            <span>Food</span>
+          </button>
         </Link>
         <Link to="/dog/treat">
-          <button>Treats</button>
+          <button className="category-button">
+            <img
+              src="https://cdn.shopify.com/s/files/1/1149/5008/files/2_3ec06e34-7868-47d7-86c3-58e041c9be0c.png"
+              alt="Treats"
+            />
+            <span>Treats</span>
+          </button>
         </Link>
         <Link to="/dog/healthcare">
-          <button>Healthcare</button>
+          <button className="category-button">
+            <img
+              src="https://cdn.shopify.com/s/files/1/1149/5008/files/dog-skin-relief.png"
+              alt="Healthcare"
+            />
+            <span>Healthcare</span>
+          </button>
         </Link>
       </div>
-    </>
+    </div>
   );
 }
