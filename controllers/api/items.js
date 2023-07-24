1af565836d61710e4998e8dedcfe322e683b5ccf
damienchoojl <damienchoojl@gmail.com>
2023-07-24 11:03:44 +0800

    created routes and controller to handle addReview

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -1,3 +1,4 @@
+const { async } = require("q");
 const Item = require("../../models/item");
 // const Account = require("../../models/account");
 // const User = require("../../models/user");
@@ -15,6 +16,28 @@ const index = async (req, res) => {
   }
 };
 
+const addReview = async (req, res) => {
+  const { itemId } = req.params;
+  const { rating, comment } = req.body;
+
+  try {
+    const item = await Item.findById(itemId);
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    // Add the new review to the item's comments array
+    item.comments.push({ userId: req.user._id, rating, comment });
+    await item.save();
+
+    res.status(200).json({ message: "Review added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to add review" });
+  }
+};
+
 module.exports = {
   index,
+  addReview,
 };
--- routes/api/items.js
+++ routes/api/items.js
@@ -4,5 +4,6 @@ const itemsController = require("../../controllers/api/items");
 
 // GET /api/items
 router.get("/", itemsController.index);
+router.post("/:itemId/add-review", itemsController.addReview);
 
 module.exports = router;
