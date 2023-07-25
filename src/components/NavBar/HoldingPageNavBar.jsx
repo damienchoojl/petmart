5dd7ffb3b5cf84ff836dc6c6c409615ac74ea732
damienchoojl <damienchoojl@gmail.com>
2023-07-25 20:36:11 +0800

    update HoldingPageNavBar

--- src/components/NavBar/HoldingPageNavBar.jsx
+++ src/components/NavBar/HoldingPageNavBar.jsx
@@ -8,7 +8,7 @@ export default function HoldingPageNavBar() {
     <nav className="navbar">
       <div className="nav-left">
         <div className="nav-logo-container">
-          <Link to="/mainpage" className="nav-logo">
+          <Link to="/" className="nav-logo">
             <img
               src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/wexaikkloejtsgnng6wy"
               alt="Logo"
